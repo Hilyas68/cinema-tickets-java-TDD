11fb48f708d61f25a321b2f6640cc6794940f696
hassanliasu <htope68@gmail.com>
2024-10-05 17:38:40 +0100

    fix: accountId must be greater than zero

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -6,17 +6,21 @@ import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketServiceImpl implements TicketService {
 
-    public static final String ACCOUNT_ID_CANNOT_BE_NULL_MESSAGE = "AccountId cannot be null";
+  public static final String ACCOUNT_ID_CANNOT_BE_NULL_MESSAGE = "AccountId cannot be null";
+  public static final String ACCOUNT_ID_MUST_BE_GRATER_THAN_MESSAGE = "AccountId must be greater than zero";
 
-    /**
-     * Should only have private methods other than the one below.
-     */
+  /**
+   * Should only have private methods other than the one below.
+   */
 
-    @Override
-    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
-        if(Objects.isNull(accountId)){
-            throw new InvalidPurchaseException(ACCOUNT_ID_CANNOT_BE_NULL_MESSAGE);
-        }
+  @Override
+  public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests)
+      throws InvalidPurchaseException {
+    if (Objects.isNull(accountId)) {
+      throw new InvalidPurchaseException(ACCOUNT_ID_CANNOT_BE_NULL_MESSAGE);
+    } else if (accountId <= 0) {
+      throw new InvalidPurchaseException(ACCOUNT_ID_MUST_BE_GRATER_THAN_MESSAGE);
     }
+  }
 
 }
