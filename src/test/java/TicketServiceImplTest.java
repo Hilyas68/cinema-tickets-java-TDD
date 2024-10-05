d2fe1318567008b9de7102dadcad7c70d5c9f945
hassanliasu <htope68@gmail.com>
2024-10-05 16:18:09 +0100

    test: verify returned message when accountId is null

--- src/test/java/TicketServiceImplTest.java
+++ src/test/java/TicketServiceImplTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -21,9 +22,13 @@ public class TicketServiceImplTest {
   }
 
   @Test
-  @DisplayName("Given a null account ID then throw an exception")
+  @DisplayName("Given a null account ID then throw an exception, with message 'AccountId cannot be null'")
   public void givenNullAccountIdThrowException() {
 
-    assertThrows(InvalidPurchaseException.class, () -> service.purchaseTickets(null));
+    InvalidPurchaseException exception = assertThrows(InvalidPurchaseException.class,
+        () -> service.purchaseTickets(null));
+
+    assertEquals("AccountId cannot be null", exception.getMessage(),
+        "should return 'AccountId cannot be null'");
   }
 }
