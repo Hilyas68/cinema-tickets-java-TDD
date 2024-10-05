2e3f6c476d136ecfecd183f8a3a4b35d7574aeec
hassanliasu <htope68@gmail.com>
2024-10-05 17:19:18 +0100

    refactor: add constant for error message

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -5,6 +5,9 @@ import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketServiceImpl implements TicketService {
+
+    public static final String ACCOUNT_ID_CANNOT_BE_NULL_MESSAGE = "AccountId cannot be null";
+
     /**
      * Should only have private methods other than the one below.
      */
@@ -12,7 +15,7 @@ public class TicketServiceImpl implements TicketService {
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
         if(Objects.isNull(accountId)){
-            throw new InvalidPurchaseException();
+            throw new InvalidPurchaseException(ACCOUNT_ID_CANNOT_BE_NULL_MESSAGE);
         }
     }
 
