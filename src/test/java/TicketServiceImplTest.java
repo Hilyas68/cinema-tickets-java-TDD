11e37a001bca6fbfdead4ca8fe9916e869174477
hassanliasu <htope68@gmail.com>
2024-10-05 17:27:42 +0100

    test: accountId must be greater than zero

--- src/test/java/TicketServiceImplTest.java
+++ src/test/java/TicketServiceImplTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.dwp.uc.pairtest.TicketService;
 import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
@@ -30,5 +32,27 @@ public class TicketServiceImplTest {
 
     assertEquals("AccountId cannot be null", exception.getMessage(),
         "should return 'AccountId cannot be null'");
+  }
+
+  @Test
+  @DisplayName("Given an account ID is less than zero then throw an exception, with message 'AccountId must be greater than zero'")
+  public void givenAccountIdLessThanZeroThrowException() {
+
+    InvalidPurchaseException exception = assertThrows(InvalidPurchaseException.class,
+        () -> service.purchaseTickets(-1L));
+
+    assertEquals("AccountId must be greater than zero", exception.getMessage(),
+        "should return 'AccountId must be greater than zero'");
+  }
+
+  @Test
+  @DisplayName("Given an account ID is equal to zero then throw an exception, with message 'AccountId must be greater than zero'")
+  public void givenAccountIIsZeroThrowException(){
+
+    InvalidPurchaseException exception = assertThrows(InvalidPurchaseException.class,
+        () -> service.purchaseTickets(0L));
+
+    assertEquals("AccountId must be greater than zero", exception.getMessage(),
+        "should return 'AccountId must be greater than zero'");
   }
 }
